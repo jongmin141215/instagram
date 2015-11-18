475ea171bb9a2594a4c0f17bab80d690e9df0df5
Jongmin Kim <kjm141215@gmail.com>
2015-11-18 12:07:00 +0000

    Edit CSS to make form look nicer

--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 feature 'Pictures' do
-  scenario 'User can add a picture' do
-    user = create :user
-    visit '/users/sign_in'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-    click_button 'Add a picture'
-    expect(page).to have_content 'Description'
-    expect(page).to have_button 'Post'
+  context 'User signed in' do
+    before do
+      user = create :user
+      visit '/users/sign_in'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+      click_button 'Add a picture'
+    end
+    scenario 'User can see a form' do
+      expect(page).to have_selector 'input[type="file"]'
+      expect(page).to have_selector 'textarea'
+      expect(page).to have_button 'Post'
+    end
+
+    # scenario 'User can add a picture' do
+    #
+    # end
   end
+
 end
